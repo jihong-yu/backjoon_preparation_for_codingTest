b4cde45ad35fcf52519fc450f9f10ba91e0f75ba
HomePC <jimdac@naver.com>
2021-10-19 18:11:25 +0900

    2021-10-19

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
-def solve(n):
-    global cnt
-    global k
-    for i in arr:
-        res.append(i)
-        k += 1
-        print(res, str(k) + "번째")
-        if sum(res) == n:
-            res.pop()
-            cnt += 1
-            break
-        solve(n)
-        res.pop()
-    return cnt
+# 1. visited (탐사 했는지 여부)
+# 2. out (탐사 내용)
+# 3. 재귀 함수 시
+# - 탈출 조건 : Depth가 M일 때
+# - 탐사를 안했을 경우 진행
+# - Depth 탐색 전 : 탐사 시작(중복 제거), 탐사 내용 append
+# - Depth 탐색 후 : 다음 탐사 준비, 탐사 내용 pop
+
+N, M = map(int, input().split())
+visited = [False] * N  # 탐사 여부 check
+out = []  # 출력 내용
+index = 0
 
 
-T = int(input())
-for _ in range(T):
-    k = 0
-    n = int(input())
-    arr = [1, 2, 3]
-    res = []
-    cnt = 0
-    print(solve(n))
+def solve(depth, N, M):
+    global index
+    if depth == M:  # 탈출 조건
+        print(' '.join(map(str, out)))  # list를 str으로 합쳐 출력
+        return
+    for i in range(len(visited)):  # 탐사 check 하면서
+        if not visited[i]:  # 탐사 안했다면
+            visited[i] = True  # 탐사 시작(중복 제거)
+            out.append(i + 1)  # 탐사 내용
+            solve(depth + 1, N, M)  # 깊이 우선 탐색
+            visited[i] = False  # 깊이 탐사 완료
+            out.pop()  # 탐사 내용 제거
+
+
+solve(0, N, M)
