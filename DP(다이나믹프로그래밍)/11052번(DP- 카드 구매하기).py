47ba6f24b437fae03c9013b2b5578d22ea2dbd1f
HomePC <jimdac@naver.com>
2021-10-30 17:29:59 +0900

    2021-10-30

---
+++ DP(다이나믹프로그래밍)/11052번(DP- 카드 구매하기).py
@@ -0,0 +1,17 @@
+N = int(input())
+
+P = [0] + list(map(int, input().split()))
+dp = [0] * (N + 1)
+
+dp[1] = P[1]
+
+for i in range(1, N + 1):
+    for j in range(1, i + 1):
+        dp[i] = max(dp[i], dp[i - j] + P[j])
+
+# for i in range(1, N + 1):
+#     for j in range(1, i + 1):
+#         if dp[i] < dp[i - j] + P[j]:
+#             dp[i] = dp[i-j] + P[j]
+
+print(dp[N])
