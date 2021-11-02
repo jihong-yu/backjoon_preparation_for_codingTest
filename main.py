71d050a99b3bed1da240593303b536e36bdb8817
HomePC <jimdac@naver.com>
2021-11-02 17:25:32 +0900

    2021-11-02

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
+N = int(input())
 
-import sys
+dp = [[0]*10 for _ in range(N+1)]
+for i in range(1, 10):
+    dp[1][i] = 1
 
-input = sys.stdin.readline
+MOD = 1000000000
 
-T = int(input())
+for i in range(2, N+1):
+    for j in range(10):
+        if j == 0:
+            dp[i][j] = dp[i-1][1]
+        elif j == 9:
+            dp[i][j] = dp[i-1][8]
+        else:
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1] 
 
-dp = [[0 for _ in range(3)] for _ in range(11)]
-
-# 각각 1,2,3으로 끝나는 상황
-dp[1] = [1, 0, 0]
-dp[2] = [1, 1, 0]
-dp[3] = [2, 1, 1]
-
-for i in range(4, 11):
-    # 만약 i가 6일때
-
-    # 5에서 1,2,3으로 끝난 횟수에 1을 더해주면 6이다.
-    dp[i][0] = dp[i - 1][1] + dp[i - 1][2] + dp[i-1][0]
-    # 4에서 1,2,3으로 끝난 횟수에 2를 더해주면 6이다.
-    dp[i][1] = dp[i - 2][0] + dp[i - 2][2] + dp[i-2][1]
-    # 3에서 1,2,3으로 끝난 횟수에 3을 더해주면 6이다.
-    dp[i][2] = dp[i - 3][0] + dp[i - 3][1] + dp[i-3][2]
-
-for i in range(T):
-    n = int(input())
-    print(sum(dp[n]))
+print(dp[N])
+print(sum(dp[N]) % MOD)
