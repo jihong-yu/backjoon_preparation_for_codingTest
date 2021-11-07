eaa5a3c4b785fad81619f85654d37086632ff3d8
HomePC <jimdac@naver.com>
2021-11-07 17:23:38 +0900

    2021-11-07

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import sys
 
 input = sys.stdin.readline
+N = int(input())
 
-T = int(input())
-dp = [[0, 0, 0] for x in range(1000001)]
-dp[1] = [1, 0, 0]
-dp[2] = [1, 1, 0]
-dp[3] = [2, 1, 1]
+dp = [[0 for _ in range(10)] for _ in range(N + 1)]
 
-for i in range(4, 1000001):
-    dp[i][0] = (dp[i-1][0]+dp[i-1][1]+dp[i-1][2]) % 1000000009
-    dp[i][1] = (dp[i-2][0]+dp[i-2][1]+dp[i-2][2]) % 1000000009
-    dp[i][2] = (dp[i-3][0]+dp[i-3][1]+dp[i-3][2]) % 1000000009
+for i in range(10):
+    dp[1][i] = 1
 
-for _ in range(T):
-    n = int(input())
-    print(sum(dp[n]) % 1000000009)
+for i in range(2, N + 1):
+    for j in range(10):
+        for k in range(j, 10):
+            dp[i][j] += dp[i - 1][k]
+
+print(sum(dp[N]) % 10007)
